2b9da3d6cdf852e5349139c397f29b6f684e3695
mfahmirizaldi <fahmirizaldi1933@gmail.com>
2022-10-31 00:42:18 +0700

    Auth permission

---
+++ models/permission.js
@@ -0,0 +1,22 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Permission extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Permission.belongsToMany(models.Role, {
+        through: 'RolePermission',
+        as: 'roles',
+        foreignKey: 'perm_id'
+      });
+    }
+  };
+  
+  return Permission;
+};
